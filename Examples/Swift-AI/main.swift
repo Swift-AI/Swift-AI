665891c53a4b0b7f20ae4a0e3cfcf823ec5b2db8
collinhundley <collinhundley@gmail.com>
2015-11-19 16:58:56 -0700

    Added example project to repo

---
+++ Examples/Swift-AI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-AI
+//
+//  Created by Collin Hundley on 11/19/15.
+//  Copyright © 2015 Appsidian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
