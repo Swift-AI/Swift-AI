1c0981563eb50856c4c3592773212b0afbb29c7a
collinhundley <collinhundley@gmail.com>
2015-11-21 12:23:25 -0700

    Updated docs for XOR example

--- Examples/Swift-AI/main.swift
+++ Examples/Swift-AI/main.swift
@@ -5,10 +5,14 @@
 //  Created by Collin Hundley on 11/19/15.
 //
 
+/// A simple example of training a feed-forward neural network to function as an XOR gate.
 func xorTwoWay() {
+    
+    // Initialize network
 	let network = FFNN(inputs: 2, hidden: 2, outputs: 1,
 		learningRate: 0.1, momentum: 0.1, weights: nil)
 	
+    // Create training data
 	let inputs: [[Float]] = [
 		[0, 0],
 		[0, 1],
@@ -16,6 +20,7 @@ func xorTwoWay() {
 		[1, 1]
 	]
 	
+    // Define answers for training data
 	let answers: [[Float]] = [
 		[0],
 		[1],
@@ -23,16 +28,22 @@ func xorTwoWay() {
 		[1]
 	]
 	
-	print("before training: \(network.getWeights())")
-	try! network.train(inputs: inputs, answers: answers, testInputs: [], testAnswers: [], errorThreshold: 0.1)
-	print("after training: \(network.getWeights())")
+    // Train the network
+    // Note: Here we use `inputs` and `answers` as validation data as well, since there are only 4 possible inputs for XOR.
+	print("Weights before training: \(network.getWeights())")
+	try! network.train(inputs: inputs, answers: answers,
+        testInputs: inputs, testAnswers: answers,
+        errorThreshold: 0.001)
+	print("Weights after training: \(network.getWeights())")
 	
+    // Print the result
 	let v0 = try! network.update(inputs: inputs[0])
 	let v1 = try! network.update(inputs: inputs[1])
 	let v2 = try! network.update(inputs: inputs[2])
 	let v3 = try! network.update(inputs: inputs[3])
-	print("values: ")
-	print(v0, v1, v2, v3)
+    print("Target output: \(answers[0][0]), \(answers[1][0]), \(answers[2][0]), \(answers[3][0])")
+	print("Actual output: \(v0.first!), \(v1.first!), \(v2.first!), \(v3.first!)")
 }
 
+// Start the XOR example
 xorTwoWay()
