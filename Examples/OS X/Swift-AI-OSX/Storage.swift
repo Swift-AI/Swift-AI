a8e56be0be45b37374e31aa068263ae30f0eea49
Andrea Tullis <andrea.tullis@sharewire.net>
2015-11-24 12:25:17 +0100

    added .gitignore for Xcode and removed unneeded  imports

---
+++ Examples/OS X/Swift-AI-OSX/Storage.swift
@@ -0,0 +1,15 @@
+//
+//  Storage.swift
+//  Swift-AI-OSX
+//
+//  Created by Andrea on 11/24/15.
+//  Copyright © 2015 Appsidian. All rights reserved.
+//
+
+public protocol Storage {
+    
+    typealias ItemType
+    func write(filename : String)
+    static func read(filename: String) -> ItemType?
+
+}
