871c4b414070a3e449c92e0d03d9622b6de54b34
Andrea Tullis <andrea.tullis@sharewire.net>
2015-11-27 15:03:33 +0100

    cleaning up test stuff

--- Examples/OS X/Swift-AI-OSX/main.swift
+++ Examples/OS X/Swift-AI-OSX/main.swift
@@ -16,22 +16,4 @@
 /// Sine Wave Example - trains a FFNN to model the function [sin(10x)/2 + 1/2] in the domain [-0.5 , 0.5]
 /// The lower the error threshold, the more accurate the waveform!
 /// Caution: Lower error thresholds improve accuracy, but can take exponentially longer to train.
-import Foundation
-//sineWave(errorThreshold: 2.0)
-
-let inputs : [[Float]] = [[0,0,1],[0,1,0],[1,0,0]]
-let outputs1 : [[Float]] = [[0.3,0.3,0.4],[0.3,0.4,0.3],[0.1,0.2,0.7]]
-
-
-var errorSum : Float = 0
-for (inputIndex, input) in outputs1.enumerate() {
-    let outputs = input
-    for (outputIndex, output) in outputs.enumerate() {
-        errorSum += crossEntropyT(output, b: inputs[inputIndex][outputIndex])
-        print(errorSum)
-    }
-}
-errorSum = -errorSum
-
-print(errorSum/3)
-
+sineWave(errorThreshold: 2.0)
