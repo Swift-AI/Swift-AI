8dff27742c558b743a639828568de7e6ee3c2d75
Alan Rabelo Martins <alanrabelo@icloud.com>
2017-04-03 18:46:56 -0300

    Updated to Swift 3.1

--- Source/Storage.swift
+++ Source/Storage.swift
@@ -15,9 +15,9 @@ import Foundation
 public protocol Storage {
     
     associatedtype StorageType
-    func writeToFile(filename: String)
-    func writeToFile(url: NSURL)
-    static func fromFile(filename: String) -> StorageType?
-    static func fromFile(url: NSURL) -> StorageType?
+    func writeToFile(_ filename: String)
+    func writeToFile(_ url: URL)
+    static func fromFile(_ filename: String) -> StorageType?
+    static func fromFile(_ url: URL) -> StorageType?
 
 }
